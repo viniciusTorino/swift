c55dafcdae5ca135dd7ff6db86960b7f5225d008
viniciusTorino <43055485+viniciusTorino@users.noreply.github.com>
2018-11-07 11:34:56 -0200

    Create returnTwoValues.swift
    whatever (8

---
+++ returnTwoValues.swift
@@ -0,0 +1,37 @@
+var player : [String : String] = [:] 
+
+player["player1"] = "Scissors"
+player["player2"] = "Rock"
+
+func winnerAndLoser(player1: String?, player2: String?) -> (winner:String, loser:String){
+    guard let player1 = player1, let player2 = player2 else {
+        return ("NOBODY WON", "NOBODY WON")
+    }
+    
+    var winner, loser : String
+    
+    switch player1 {
+        case "Rock" where player2 == "Scissors":
+            winner = "player1"
+            loser = "player2"
+        case "Papel" where player2 == "Rock":
+            winner = "player1"
+            loser = "player2"
+        case "Scissors" where player2 == "Papel":
+            winner = "player1"
+            loser = "player2"
+        case player2 :
+            winner = "DRAW"
+            loser = "DRAW"
+        default:
+            //If there isn't any typo .. the only possible output beside our previous ... is that player2 won , so there is not necessary to continue testing it
+            winner = "player2"
+            loser = "player1"
+    }
+        return (winner, loser)
+    
+}
+var winnerAndLoserOfMatch =  winnerAndLoser(player1: player["player1"], player2: player["player2"])
+
+print(winnerAndLoserOfMatch)
+
